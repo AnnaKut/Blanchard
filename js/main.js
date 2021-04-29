2e8eade7f0ef8d3de80d254c039ea457fc5cf5ec
Kutaeva Anna <annakozlovabio@gmail.com>
2021-04-29 21:34:50 +0300

    Add block catalog

--- js/main.js
+++ js/main.js
@@ -10,6 +10,27 @@ window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('fixed-page');
   }
 
+  function showArtistPage(country = 'italy') {
+    const art = document.querySelectorAll(`.artists__link_${country}`);
+
+    art.forEach((artists) => {
+    artists.addEventListener('click', (event) => {
+    const id = event.currentTarget.dataset.id
+
+    document.querySelectorAll(`.catalog__artist_${country}`).forEach((artists) => {
+      artists.classList.remove('catalog__artist_active');
+    })
+    document.getElementById(`${id}`).classList.add('catalog__artist_active');
+    art.forEach((artists) => {
+      artists.classList.remove('artists__link_active')
+    })
+    document.querySelector(`[data-id="${id}"]`).classList.add('artists__link_active')
+
+    })
+  
+    })
+  }
+
   document.querySelector('#burger').addEventListener('click', () => {
       menu.classList.toggle(classMenu);
       enter.classList.toggle(classMenu);
@@ -23,6 +44,34 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.main-header__enter-link').addEventListener('click', () => {
     removeMenu();
   });
+
+  const tab = document.querySelectorAll('.catalog__btn');
+
+  tab.forEach((tabs) => {
+    tabs.addEventListener('click', (event) => {
+      const path = event.currentTarget.dataset.path;
+      document.querySelectorAll('.catalog__content').forEach(function(tabContent) {
+        tabContent.classList.remove('catalog__content_active');
+      })
+      document.querySelector(`[data-target="${path}"]`).classList.toggle('catalog__content_active');
+      tab.forEach((tabs) => {
+        tabs.classList.remove('catalog__btn_active');
+      })
+      document.querySelector(`[data-path="${path}"]`).classList.add('catalog__btn_active');
+      showArtistPage(path);
+
+      document.querySelectorAll(".accordion").forEach((el) => {
+        new Accordion(el, {
+        modal: true,
+        });
+      });
+    })
+  });
+
+  showArtistPage();
+
+  
+  
 });
 
 document.addEventListener('DOMContentLoaded', () => {
